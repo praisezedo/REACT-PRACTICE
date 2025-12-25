011f33011e5ee6e0d9ae0d7009f87aaf855aee0a
Praise Innocent <praiseinnocent049@gmail.com>
2025-12-25 06:30:36 +0100

    react clock

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState , useEffect} from "react";
+function App() {
+    const [time , settime] = useState(new Date());
+    useEffect(() => {
+        const timer = setInterval(() => {
+            settime(new Date())
+        } , 1000);
+        return () => clearInterval(timer);
+    } , []);
+   function formattime (time) {
+    return time.toString().padStart(2 , '0');
+   }
+const hours = time.getHours();
+const minutes = time.getMinutes();
+const seconds = time.getSeconds();
+const ampm = time.getHours() >= 12 ? 'PM' : 'AM';
+return (
+    <>
+    <div className = "border-3 text-center w-150 mx-auto rounded-2xl  min-h-100 mt-20  border-black items-center justify-center flex flex-col">
+    <h1 className="font-bold text-4xl my-7">REAL CLOCK</h1>
+    <div className="flex flex-col">
+    <h1 className="font-bold text-4xl my-7">{`${formattime(hours)}:${formattime(minutes)}:${formattime(seconds)}${ampm}`}</h1>
+  <h2 className="font-bold text-4xl my-7">{time.toDateString()}</h2>
+    </div>
+    </div>
+    </>
+)
+}
+export default App;
+
+
+
+
+ 
---
+++ src/Calculator.jsx
@@ -0,0 +1,17 @@
+ function Calculator() {
+    function appendToDisplay() {
+
+    }
+    function result() {
+
+    }
+    function Delete() {
+
+    }
+    function clearinput () {
+        
+    }
+    return {
+      
+    };
+ }
